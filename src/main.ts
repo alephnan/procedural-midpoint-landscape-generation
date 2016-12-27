16a4bc7e5d19550e7764b15dccdec876feef2d99
automatwon <automatwon@gmail.com>
2016-12-27 00:56:27 -0800

    Add basic background and line

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@
 import { MidpointDisplacer } from './midpoint-displacer';
 
 const sketch = function (p : p5) {
-  const W = 600;
-  const H = 400;
-  
+  const W : number = 600;
+  const H : number = 400;
+
   p.setup = function () {
     p.createCanvas(W, H);
+
+    p.background(p.color('yellow'));
+
+    p.stroke(p.color('red'));
+    p.strokeWeight(2);
+    p.line(0, H/2, W, H/2);
   };
 };
 
