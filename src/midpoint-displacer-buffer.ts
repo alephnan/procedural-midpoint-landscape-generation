916fb110ca79f159f4bb21406e704853e1392c98
automatwon <automatwon@gmail.com>
2016-12-27 01:19:58 -0800

    Add a iniitial line segment in MidpointDisplacerBuffer with placeholder render method

--- src/midpoint-displacer-buffer.ts
+++ src/midpoint-displacer-buffer.ts
@@ -1,11 +1,25 @@
 /**
  * Data Structure for set of line segments generated via midpoint displacement
  */
+import {Pair} from './pair';
 import {Line} from './line';
 
 export class MidpointDisplacerBuffer {
+  // TODO(automatwon): this is probably more efficiently represented as sequence
+  // of points. But easier to implement with Lines for now
   private lineSegments: Array<Line>;
-  constructor(initialResolution: number) {
+  constructor(initialResolution: number, w: number, h: number) {
     this.lineSegments = new Array();
+
+    const p1 : Pair = new Pair(0, 0);
+    const p2 : Pair = new Pair(w, h);
+    const l : Line = new Line(p1, p2);
+    this.lineSegments.push(l);
+  }
+
+  public render(p: p5) {
+    this.lineSegments.forEach((l: Line) => {
+      console.log("Line: " + l.toString());
+    });
   }
 }
