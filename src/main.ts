a48efa15976bb39a1d46954c624749786a454967
automatwon <automatwon@gmail.com>
2016-12-31 02:07:00 -0800

    Vertical align graph

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import {TerrainGenerator} from './terrain-generator';
 import { ScrollingMidpointDisplacerLinkedList } from './scrolling-midpoint-displacer-linkedlist';
 
 const sketch = function (p : p5) {
+  const MARGIN_PERCENTAGE : number = .15;
+  
   const W : number = 2000;
   const H : number = 400;
 
   const minimumW: number = 640;
-  const maximumH: number = Math.round(H *.8);
+  const maximumH: number = Math.round(H *(1 - MARGIN_PERCENTAGE));
 
   const mdp : TerrainGenerator = new ScrollingMidpointDisplacerLinkedList(minimumW, maximumH);
 
@@ -16,14 +18,18 @@ const sketch = function (p : p5) {
     p.createCanvas(W, H);
     p.strokeWeight(2);
     p.frameRate(20);
+    
   };
 
   p.draw = function() {
     p.background(p.color('black'));
-
+    
+    p.push();
+    p.translate(0, MARGIN_PERCENTAGE * H / 2);
     p.stroke(p.color(98, 203, 157));
     mdp.update(); 
     mdp.render(p);
+    p.pop();
 
     p.stroke(p.color('white'));
     p.line(minimumW, 0, minimumW, H);
